91bf04f2159f0c963e0ffe74fff37c39e7e08fda
Hasan Ibraheem <hasantiny@gmail.com>
2019-02-16 00:15:18 -0500

    Unicode 11.0 update

--- src/lib.rs
+++ src/lib.rs
@@ -9,15 +9,16 @@ include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 pub fn get_name(c: u32) -> &'static str {
     match UNICODE.get(&c) {
         Some(s) => s,
+        // TODO: Make this automatic?
         None => match c {
             0x3400...0x4DB5 => "CJK Ideograph Extension A",
-            0x4E00...0x9FD5 => "CJK Ideograph",
+            0x4E00...0x9FEF => "CJK Ideograph",
             0xAC00...0xD7A3 => "Hangul Syllable",
             0xD800...0xDB7F => "Non Private Use High Surrogate",
             0xDB80...0xDBFF => "Private Use High Surrogate",
             0xDC00...0xDFFF => "Low Surrogate",
             0xE000...0xF8FF => "Private Use",
-            0x17000...0x187EC => "Tangut Ideograph",
+            0x17000...0x187F1 => "Tangut Ideograph",
             0x20000...0x2A6D6 => "CJK Ideograph Extension B",
             0x2A700...0x2B734 => "CJK Ideograph Extension C",
             0x2B740...0x2B81D => "CJK Ideograph Extension D",
